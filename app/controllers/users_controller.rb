64ec65fcc5bd72e49f84cbb8cc5bcaac75183ecf
mariel-delagarza <mariel.code@gmail.com>
2019-06-07 00:28:56 -0400

    Add create action that can handle signup failure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user]) #not final implementation
+    if @user.save
+      # Handle successful save
+    else 
+      render 'new'
+    end
+  end
   
 end
