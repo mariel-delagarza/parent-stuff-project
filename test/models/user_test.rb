958927eac365e5e58e8a6a1e509fbe2a2cfc30c8
mariel-delagarza <mariel.code@gmail.com>
2019-06-06 21:22:18 -0400

    Update email uniqueness test for case insensitivity

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -48,8 +48,9 @@ class UserTest < ActiveSupport::TestCase
 
   test "email addresses should be unique" do
     duplicate_user = @user.dup 
+    duplicate_user = @user.email.upcase 
     @user.save
     assert_not duplicate_user.valid?
   end
-  
+
 end
