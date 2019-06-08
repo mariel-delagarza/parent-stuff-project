b1c6435650be27289d9ff513c2972fe54532a113
mariel-delagarza <mariel.code@gmail.com>
2019-06-07 20:48:54 -0400

    Implement strong params with private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user]) #not final implementation
+    @user = User.new(user_params)
     if @user.save
       # Handle successful save
     else 
@@ -17,4 +17,10 @@ class UsersController < ApplicationController
     end
   end
   
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
