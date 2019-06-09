1447ffc5cc7a0ef35a9a381b33bf4702495ffe3b
mariel-delagarza <mariel.code@gmail.com>
2019-06-08 21:38:44 -0400

    Correct syntax errors in tests to redirect edit and update when not logged in

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -14,14 +14,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should redirect edit when not logged in" do
     get edit_user_path(@user)
     assert_not flash.empty?
-    assert_redirect_to login_url
+    assert_redirected_to login_url
   end
 
   test "should redirect update when not logged in" do
-    patch user_path(@user), params { user: { name: @user.name, 
+    patch user_path(@user), params: { user: { name: @user.name, 
                                              email: @user.email } }
     assert_not flash.empty?
-    assert_redirect_to login_url
+    assert_redirected_to login_url
   end 
-  
+
 end
