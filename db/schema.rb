187264e08593c9e2162b6f201eb2664ec964717e
mariel-delagarza <mariel.code@gmail.com>
2019-06-09 12:31:46 -0400

    Run migration, update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_09_161404) do
+ActiveRecord::Schema.define(version: 2019_06_09_163049) do
 
   create_table "places", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_06_09_161404) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "place_id"
   end
 
   create_table "users", force: :cascade do |t|
