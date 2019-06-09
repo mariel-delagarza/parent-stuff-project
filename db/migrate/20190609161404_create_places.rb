8297b98027e918148329f3931861c51eb0498a62
mariel-delagarza <mariel.code@gmail.com>
2019-06-09 12:20:47 -0400

    Added columns to places table before migrating

--- db/migrate/20190609161404_create_places.rb
+++ db/migrate/20190609161404_create_places.rb
@@ -1,7 +1,12 @@
 class CreatePlaces < ActiveRecord::Migration[5.2]
   def change
     create_table :places do |t|
-
+      t.string :name 
+      t.string :address 
+      t.string :phone_number 
+      t.string :website 
+      t.string :category 
+      
       t.timestamps
     end
   end
