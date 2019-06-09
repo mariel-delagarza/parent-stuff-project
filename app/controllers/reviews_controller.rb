0d8a8667b533b5ae0cd510a775d63d463072613e
mariel-delagarza <mariel.code@gmail.com>
2019-06-09 10:44:05 -0400

    Update create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,13 @@ class ReviewsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
+    @review = current_user.reviews.build(review_params)
+    if @review.save
+      flash[:success] = "Review created!"
+      redirect_to root_url
+    else      
+      render 'static_pages/home'
+    end
   end
 
   def destroy
