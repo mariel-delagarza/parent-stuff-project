d14084107387c73a006f48c5c885e2ef928021f0
mariel-delagarza <mariel.code@gmail.com>
2019-06-09 16:51:05 -0400

    Add association between Place and Feature models

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,7 @@ class Place < ApplicationRecord
   #has_many :features 
   has_many :reviews
   has_many :users, through: :reviews 
+  has_many :features
 
   validates :name, presence: true
   validates :address, presence: true, uniqueness: true 
