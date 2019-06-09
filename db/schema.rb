1862cb1a21779641d3783ecd44fd8eb18f740fb6
mariel-delagarza <mariel.code@gmail.com>
2019-06-09 01:20:36 -0400

    Use generator to create Reviews controller, reset and re-seeded db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_09_031918) do
+ActiveRecord::Schema.define(version: 2019_06_09_051049) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
