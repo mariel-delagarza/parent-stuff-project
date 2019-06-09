beb85661ccd52c2b467b37e607bc0778ca2656ce
mariel-delagarza <mariel.code@gmail.com>
2019-06-09 16:48:18 -0400

    Add columns to features table before migrating

--- db/migrate/20190609203338_create_features.rb
+++ db/migrate/20190609203338_create_features.rb
@@ -1,7 +1,11 @@
 class CreateFeatures < ActiveRecord::Migration[5.2]
   def change
     create_table :features do |t|
-
+      t.boolean :changing_tables
+      t.boolean :family_bathroom
+      t.boolean :high_chairs
+      t.boolean :nursing_room
+      
       t.timestamps
     end
   end
