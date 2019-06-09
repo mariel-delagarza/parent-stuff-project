e4ee2cca8abe96e234851b7c561bcf35aa763713
mariel-delagarza <mariel.code@gmail.com>
2019-06-08 23:21:42 -0400

    Run migration adding users admin attribute to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_015552) do
+ActiveRecord::Schema.define(version: 2019_06_09_031918) do
+
+  create_table "features", force: :cascade do |t|
+    t.boolean "changing_tables"
+    t.boolean "family_bathroom"
+    t.boolean "high_chairs"
+    t.boolean "nursing_room"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone_number"
+    t.string "website"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "category"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_06_07_015552) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
