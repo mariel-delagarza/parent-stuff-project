b0084adaf59f37ebefb5b243cdf062489be7f9f3
mariel-delagarza <mariel.code@gmail.com>
2019-06-08 21:03:18 -0400

    Add test for unsuccesful profile edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:waffles)
+  end
+
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: "", 
+                                              email: "dog@badboy",
+                                              password:              "bad",
+                                              password_confirmation: "boy" } }
+    assert_template 'users/edit'
+  end 
+
 end
