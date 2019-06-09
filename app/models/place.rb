a7695088a4d42f6dd57ecb72f38363be620c7606
mariel-delagarza <mariel.code@gmail.com>
2019-06-09 12:33:36 -0400

    Correct syntax error in has_many through association

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ApplicationRecord
   #has_many :features 
   has_many :reviews
-  has_many :users, through :reviews 
+  has_many :users, through: :reviews 
 
   validates :name, presence: true
   validates :address, presence: true, uniqueness: true 
