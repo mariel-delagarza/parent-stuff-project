04779e5611a31beb894b5319ae70b1a2867b3b6e
mariel-delagarza <mariel.code@gmail.com>
2019-06-09 11:32:43 -0400

    Add destroy action allowing logged in users to delete their own reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @review = current_user.reviews.build(review_params)
@@ -13,12 +14,20 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
+    @review.destroy
+    flash[:success] = "Review deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def review_params
       params.require(:review).permit(:content)
+    end
+
+    def correct_user
+      @review = current_user.reviews.find_by(id: params[:id])
+      redirect_to root_url if @review.nil?
     end
 
 end
