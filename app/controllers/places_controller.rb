1d9bdb7c3ea8002902caece9351b648a4e27d4cd
mariel-delagarza <mariel.code@gmail.com>
2019-06-09 14:06:54 -0400

    Create place index action and associated view; create a places partial to use in places index view; update CSS to include CSS for places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,7 @@
 class PlacesController < ApplicationController
 
   def index
+    @places = Place.paginate(page: params[:page])
   end
   
   def new
@@ -8,6 +9,13 @@ class PlacesController < ApplicationController
   end
 
   def create  
+    @place = Place.new(place_params)
+    if @place.save
+      flash[:success] = "Your new place was created!"
+      redirect_to places_path
+    else 
+      render 'new'
+    end
   end
   
   def edit  
@@ -18,5 +26,12 @@ class PlacesController < ApplicationController
 
   def destroy
   end
+
+  private
+  
+  def place_params
+    params.require(:place).permit(:name, :address, :website, :phone_number, :category)
+  end
+
 
 end
