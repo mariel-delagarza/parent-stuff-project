8107674d8d29ad82e6984cdb23fa1f01130c3e82
mariel-delagarza <mariel.code@gmail.com>
2019-06-09 12:50:16 -0400

    Add CRUD actions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,21 @@
 class PlacesController < ApplicationController
+
+  def index
+  end
+  
+  def new
+  end
+
+  def create  
+  end
+  
+  def edit  
+  end
+
+  def update  
+  end
+
+  def destroy
+  end
+
 end
