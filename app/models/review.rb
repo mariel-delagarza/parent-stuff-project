510f52354eba046f7a08acb27deda7a57b0d0078
mariel-delagarza <mariel.code@gmail.com>
2019-06-09 12:22:56 -0400

    Added belongs_to association between Review and Place

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
+  belongs_to :place
+
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
