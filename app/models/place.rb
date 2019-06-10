8005a25b08dd519cc05c5dba4c236d0be4a13788
mariel-delagarza <mariel.code@gmail.com>
2019-06-09 20:32:40 -0400

    Remove features association after dropping features table

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,6 @@
-class Place < ApplicationRecord
-  #has_many :features 
+class Place < ApplicationRecord 
   has_many :reviews
   has_many :users, through: :reviews 
-  has_many :features
 
   validates :name, presence: true
   validates :address, presence: true, uniqueness: true 
