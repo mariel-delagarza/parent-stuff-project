540daf3cc0edd2773f7a279ee39de36419198569
mariel-delagarza <mariel.code@gmail.com>
2019-06-09 20:28:07 -0400

    Created migration to drop features table after adding features columns to places table

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_09_203338) do
-
-  create_table "features", force: :cascade do |t|
-    t.boolean "changing_tables"
-    t.boolean "family_bathroom"
-    t.boolean "high_chairs"
-    t.boolean "nursing_room"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2019_06_10_002700) do
 
   create_table "places", force: :cascade do |t|
     t.string "name"
@@ -29,6 +20,10 @@ ActiveRecord::Schema.define(version: 2019_06_09_203338) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "changing_tables"
+    t.boolean "family_bathroom"
+    t.boolean "high_chairs"
+    t.boolean "nursing_room"
   end
 
   create_table "reviews", force: :cascade do |t|
